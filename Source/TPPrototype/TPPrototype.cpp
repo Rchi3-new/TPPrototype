38b9d83200e4f4091e39fca03ab97095918309c7
Archie2282 <gtuchkov@me.com>
2025-03-11 00:07:52 +0300

    1st commit - testlevel created

---
+++ Source/TPPrototype/TPPrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TPPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPPrototype, "TPPrototype" );
